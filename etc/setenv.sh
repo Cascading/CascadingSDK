07983bb5f58e15f16065e44fd744c6fe4303c164
Andre Kelpe <efeshundertelf@googlemail.com>
2014-01-29 15:05:16 -0800

    added possibility to install driven from the SDK

--- etc/setenv.sh
+++ etc/setenv.sh
@@ -1,7 +1,12 @@
 
 [ -z "${CASCADING_SDK_HOME}" ] && echo "CASCADING_SDK_HOME must be set" && return
 
-PATH=@BIN_PATHS@:$PATH
+PATH=${CASCADING_SDK_HOME}/tools/multitool-20131119/bin:${CASCADING_SDK_HOME}/tools/load-20131119/bin:${CASCADING_SDK_HOME}/tools/lingual-client/bin:$PATH
 
-#for en in $CASCADING_SDK_HOME/etc/env.d/*.sh ; do . $en ; done
-#unset en
+if [ ! -d $HOME/.cascading/.driven-plugin  ]; then
+    echo -e "\n-------------------------------------------------------------------------------------"
+    echo -e "Get Driven for Cascading by running $CASCADING_SDK_HOME/etc/get-driven.sh"
+    echo -e "For more information go to http://cascading.io/"
+    echo -e "-------------------------------------------------------------------------------------"
+fi
+
